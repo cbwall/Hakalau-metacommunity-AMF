7f1a69fc178b40d61d2dd6ff45b2cb521f8ada39
Chris Wall <chriswall@dhcp-168-105-211-113.wireless.manoa.hawaii.edu>
2019-10-23 16:04:11 -1000

    updates

--- scripts/SpiecEasiBasics.R
+++ scripts/SpiecEasiBasics.R
@@ -89,36 +89,46 @@ legend('topright',legend=levels(Group), pch=16, col=pal, border=NA, box.lty=0, b
 ### RO (Remnant Forest) sites
 
 ## subset otu table
+# rows as OTUs, columns as samples
 ROdat<-otu[,grep("^RO", colnames(otu))] # data with only RO
 
 # subset RO matrix, ESVs in >2 samples, add pseudo value (1), transpose, relative abundance
 RO.otu<-as.matrix(ROdat[-which(rowMeans(ROdat) < 2),])
 RO.otu.pc <- (RO.otu)+1
-RO.otu.tss <- t(apply(RO.otu.pc, 1, norm_to_total))
+
+# this transposes data 2x, keeps rows as OTUs
+RO.otu.tss <- t(apply(RO.otu.pc, 1, norm_to_total)) 
+
+### BUT the columns need to be OTUs!!!
 
 # pair rownames from OTU with the taxonomy from 'taxmat'
 RO.tax<-merge(RO.otu.tss, taxmat, by = "row.names", all = TRUE) # merge dataframes
+RO.tax<-droplevels(RO.tax)
 RO.tax<-na.omit(RO.tax)
 RO.tax.names<- RO.tax %>% dplyr::select(Kingdom, Phylum,  Class, Order,  Family,  Genus, Species, Name)
 
+## now transpose the otus in analysis below so that OTUs are columns, samples rows
+RO.otu.SE<-t(RO.otu.tss)
 
 # Models with glassofitting
-RO.otu.est.SpEa.GL <- spiec.easi(RO.otu.tss, method='glasso', pulsar.params = list(thresh = 0.1)) 
-RO.otu.est.SpEa.MB <- spiec.easi(RO.otu.tss, method='mb', pulsar.params = list(thresh = 0.1)) 
+RO.otu.est.SpEa.GL <- spiec.easi(RO.otu.SE, method='glasso', pulsar.params = list(thresh = 0.1)) 
+RO.otu.est.SpEa.MB <- spiec.easi(RO.otu.SE, method='mb', pulsar.params = list(thresh = 0.1)) 
 
 # Create igraph objects
 igRO.gl <- adj2igraph(getRefit(RO.otu.est.SpEa.GL))
 igRO.mb <- adj2igraph(getRefit(RO.otu.est.SpEa.MB))
 
 ## set size of vertex proportional to clr-mean
-RO.vsize    <- rowMeans(clr(RO.otu.tss, 1))+6
+RO.vsize    <- rowMeans(clr(RO.otu.SE, 1))+6
 RO.am.coord <- layout.fruchterman.reingold(igRO.mb)
 
 # Lets look at the degree statistics from the networks inferred by each method.
 dd.ROgl     <- degree.distribution(igRO.gl)
 dd.ROmb     <- degree.distribution(igRO.mb)
 
-########### Divide up: AK sites
+
+
+########### Divide up: AK sites 
 # AK
 
 ## subset otu table
@@ -127,23 +137,28 @@ AKdat<-otu[,grep("^AK", colnames(otu))] # data with only RO
 # subset AK matrix, ESVs in >2 samples, add pseudo value (1), transpose, relative abundance
 AK.otu<-as.matrix(AKdat[-which(rowMeans(AKdat) < 2),])
 AK.otu.pc <- (AK.otu)+1
-AK.otu.tss <- t(apply(AK.otu.pc, 1, norm_to_total))
+# this transposes data 2x, keeps rows as OTUs
+AK.otu.tss <- t(apply(AK.otu.pc, 1, norm_to_total)) 
 
 # pair rownames from OTU with the taxonomy from 'taxmat'
 AK.tax<-merge(AK.otu.tss, taxmat, by = "row.names", all = TRUE) # merge dataframes
+AK.tax<-droplevels(AK.tax)
 AK.tax<-na.omit(AK.tax)
 AK.tax.names<- AK.tax %>% dplyr::select(Kingdom, Phylum,  Class, Order,  Family,  Genus, Species, Name)
 
+## now transpose the otus in analysis below so that OTUs are columns, samples rows
+AK.otu.SE<-t(AK.otu.tss)
+
 # -------------------------- Models
-AK.otu.est.SpEa.GL <- spiec.easi(AK.otu.tss, method='glasso', pulsar.params = list(thresh = 0.1))
-AK.otu.est.SpEa.MB <- spiec.easi(RO.otu.tss, method='mb', pulsar.params = list(thresh = 0.1)) 
+AK.otu.est.SpEa.GL <- spiec.easi(AK.otu.SE, method='glasso', pulsar.params = list(thresh = 0.1))
+AK.otu.est.SpEa.MB <- spiec.easi(AK.otu.SE, method='mb', pulsar.params = list(thresh = 0.1)) 
 
 # Create igraph objects
 igAK.gl <- adj2igraph(getRefit(AK.otu.est.SpEa.GL))
 igAK.mb <- adj2igraph(getRefit(AK.otu.est.SpEa.MB))
 
 ## set size of vertex proportional to clr-mean
-AK.vsize    <- rowMeans(clr(AK.otu.tss, 1))+6
+AK.vsize    <- rowMeans(clr(AK.otu.SE, 1))+6
 AK.am.coord <- layout.fruchterman.reingold(igAK.mb)
 
 # Lets look at the degree statistics from the networks inferred by each method.
@@ -156,46 +171,59 @@ dd.AKmb     <- degree.distribution(igAK.mb)
 ## networks
 
 pal <- brewer.pal(10, "BrBG")
-GroupRO <- RO.tax.names$Family
+GroupRO <- t(RO.tax.names$Family)
 vertex.col <- pal[GroupRO]
 par(mfrow=c(1,1), mar=c(0,1,1,1))
 
 plot(igRO.gl, layout=RO.am.coord, vertex.size=RO.vsize, vertex.label=NA, main="Remnant Forest", 
      vertex.color=vertex.col, col=1:10) #"#336B87"
-legend('topleft',legend=levels(GroupRO), pch=16, col=pal, border=NA, box.lty=0, bg="transparent", 
-       cex=0.8, pt.cex=1.2, y.intersp = 0.6, inset=c(0.65, 0.1))
+legend('topright',legend=levels(GroupRO), pch=16, col=pal, border=NA, box.lty=0, bg="transparent", 
+       cex=0.8, pt.cex=1.2, y.intersp = 0.6, inset=c(0.6, 0.025))
 dev.copy(pdf, "figures/ROnetwork.pdf", height=6, width=7)
 dev.off() 
 
 
-GroupAK <- AK.tax.names$Family
+GroupAK <- t(AK.tax.names$Family)
 vertex.col <- pal[GroupAK]
 plot(igAK.gl, layout=AK.am.coord, vertex.size=AK.vsize, vertex.label=NA, main="Restored Forest", 
      vertex.color=vertex.col, col=1:10) #"#336B87"
-legend('bottomright',legend=levels(GroupAK), pch=16, col=pal, border=NA, box.lty=0, bg="transparent", 
-       cex=0.8, pt.cex=1.2, y.intersp = 0.6, inset=c(0.52, 0.05))
+legend('topright',legend=levels(GroupAK), pch=16, col=pal, border=NA, box.lty=0, bg="transparent", 
+       cex=0.8, pt.cex=1.2, y.intersp = 0.6, inset=c(0.6, 0.05))
 dev.copy(pdf, "figures/AKnetwork.pdf", height=6, width=7)
 dev.off() 
 
 
-
 ### degree distance
-par(mfrow=c(1,2), mar=c(3,1,1,2))
+par(mfrow=c(1,2), mar=c(3,3,2,2))
 
 # RO degree distribution
 plot(0:(length(dd.ROgl)-1), dd.ROgl, col="red" , type='b', ylab="Frequency", xlab="Degree", 
-     ylim=c(0,0.2), xlim=c(0,30), main="RO Degree Distributions")
+     ylim=c(0,0.2), xlim=c(0,20), main="RO Degree Distributions")
 points(0:(length(dd.ROmb)-1), dd.ROmb, col="forestgreen", type='b')
 
 # AK degree distribution
 plot(0:(length(dd.AKgl)-1), dd.AKgl, col="red" , type='b', ylab="Frequency", xlab="Degree",
-     ylim=c(0,0.2), xlim=c(0,30), main="AK Degree Distributions")
+     ylim=c(0,0.2), xlim=c(0,20), main="AK Degree Distributions")
 points(0:(length(dd.AKmb)-1), dd.AKmb, col="forestgreen", type='b')
 legend("topleft", c("MB", "glasso"), box.lty=0, bg="transparent", y.intersp = 0.4,
        col=c("forestgreen", "red"), pch=1, lty=1)
 
 dev.copy(pdf, "figures/network.dd.pdf", height=5, width=7)
 dev.off() 
+
+##########
+
+# Lets look at the degree statistics from the networks inferred by each method.
+d.AKgl<-as.data.frame(degree(igAK.gl, mode = "total")); colnames(d.AKgl)<-"degree"
+d.ROgl <- as.data.frame(degree(igRO.mb, mode = "total")); colnames(d.ROgl)<-"degree"
+
+par(mfrow=c(1,1))
+plot(0:(length(d.AKgl$degree)-1), d.AKgl$degree, pch=21, col="red", cex=0.5)
+points(0:(length(d.ROgl$degree)-1), d.ROgl$degree, col="forestgreen", pch=21, cex=0.5)
+legend("topleft", c("RO", "AK"), box.lty=0, bg="transparent", y.intersp = 0.4,
+       col=c("red", "forestgreen"), pch=1, lty=1)
+
+
 
 
 
